5f5c2273c85512c4b28a57ca9b60250951e7c272
Luis Frisneda <luis.frisneda@vnstudios.com>
2025-12-23 14:26:36 -0400

    Add target to button on order index

---
+++ extensions/account-extension/src/OrderIndexBlock.jsx
@@ -0,0 +1,36 @@
+import {
+  BlockStack,
+  Button,
+  reactExtension,
+  useApi,
+} from "@shopify/ui-extensions-react/customer-account";
+import { Grid } from "@shopify/ui-extensions/checkout";
+
+export default reactExtension(
+  "customer-account.order-index.block.render",
+  () => <WineOptions />
+);
+
+function WineOptions() {
+  const { sessionToken } = useApi();
+
+  const handleViewInventory = () => {
+    window.location.href = "/tools/storage-inventory";
+  };
+
+  const handleSelectWines = () => {
+    window.location.href = "/tools/select-wines";
+  };
+
+  return (
+    <BlockStack spacing="loose">
+      <Grid
+        columns={['1fr', '1fr']}
+        spacing="loose"
+      >
+        <Button onPress={handleSelectWines}>Select Wines for Shipping</Button>
+        <Button onPress={handleViewInventory}>Show Storage Inventory</Button>
+      </Grid>
+    </BlockStack>
+  );
+}
